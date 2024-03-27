8af76ee045bde2b0858ec4980c7ca4f3e293a8d6
ub9fbi <zolotoianatolji@yandex.com>
2024-03-27 12:15:42 +0500

    Реализован класс Matrix

--- src/Main.java
+++ src/Main.java
@@ -3,63 +3,73 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите количество строк матрицы (n)");
         int n = scanner.nextInt();
 
-        System.out.println("Ввведите количество столбцов матрицы (m)");
+        System.out.println("Введите количество столбцов матрицы (m)");
         int m = scanner.nextInt();
 
-        Matrix matrix = new Matrix();
-        matrix.inputMatrix(n,m);
-
+        Matrix matrix = new Matrix(n, m);
+        matrix.inputMatrix();
+        matrix.transposeMatrix();
+        matrix.printTransposedMatrix();
+        matrix.sumMatrixElements();
     }
 }
 
 class Matrix {
 
-    public int n,m;
-    int matrix [][]  = new int[n][m];
-    public Matrix() {
+    public int n, m;
+    public int[][] matrix;
 
+    public Matrix(int n, int m) {
+        this.n = n;
+        this.m = m;
+        matrix = new int[n][m];
     }
+
     // ввод элементов матрицы
-
-
-    public void inputMatrix(int n,int m){
+    public void inputMatrix() {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите элементы матрицы");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print("Строка матрицы " + i + " номер элемента: " + j + " : ");
-                matrix [i][j] = scanner.nextInt();
+                matrix[i][j] = scanner.nextInt();
             }
         }
     }
+
     // Транспонированная матрица
-    public void transMatrix(int matrix [][]){
-        System.out.println("=== Транспонированая матрица ===");
-        int transponMatrix [][]  = new int[m][n];
+    public void transposeMatrix() {
+        int[][] transposedMatrix = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                transponMatrix[i][j] = matrix[j][i];
-                System.out.print(transponMatrix[i][j] + " ");
+                transposedMatrix[i][j] = matrix[j][i];
+            }
+        }
+        matrix = transposedMatrix; // заменяем изначальную матрицу на транспонированную
+    }
+
+    // Вывод транспонированной матрицы
+    public void printTransposedMatrix() {
+        System.out.println("Транспонированная матрица:");
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + " ");
             }
             System.out.println();
         }
-
     }
-    // суммирование матрицы
-    public void sumMatrixElements(int matrix [][]) {
+
+    // Сумма элементов матрицы
+    public void sumMatrixElements() {
         int sum = 0;
-        for (int[] row : matrix){
+        for (int[] row : matrix) {
             for (int element : row) {
                 sum += element;
             }
         }
+        System.out.println("Сумма элементов матрицы: " + sum);
     }
 }
-
-
-
